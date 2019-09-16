267e76c1ced5995ada59520404cd079b291d4f4a
ghoshs <ghoshs@mpi-inf.mpg.de>
2019-09-16 17:20:07 +0200

    features for inverse predicates

--- feature_file/create_feature_file.R
+++ feature_file/create_feature_file.R
@@ -10,17 +10,28 @@ data <- rbind(data, read.csv('pred_property_p_50/wd.csv'))
 data <- rbind(data, read.csv('pred_property_p_50/fb.csv'))
 data$predicate <- as.character(data$predicate)
 
+### get inverse predicate properties
+data_inv <- read.csv('pred_property_p_50/dbp_map_inv.csv')
+data_inv <- rbind(data_inv, read.csv('pred_property_p_50/dbp_raw_inv.csv'))
+data_inv <- rbind(data_inv, read.csv('pred_property_p_50/wd_inv.csv'))
+data_inv <- rbind(data_inv, read.csv('pred_property_p_50/fb_inv.csv'))
+data_inv$pred_inv <- as.character(data_inv$pred_inv)
+
 ### get predicate usage data
 est_matches <- read.csv('predicate_usage_features/estimated_matches.csv')
 est_matches[] <- lapply(est_matches, function(x) if (is.factor(x)) as.character(x) else {x})
 
 data <- inner_join(data, est_matches, by='predicate')
+data_inv <- inner_join(data_inv, est_matches, by=c('pred_inv' = 'predicate'))
 
 ### get predicate subject/object types
 sub_obj_types <- read.csv('predicate_usage_features/sub_obj_types.csv')
 sub_obj_types[] <- lapply(sub_obj_types, function(x) if (is.factor(x)) as.character(x) else {x})
 
 data <- inner_join(data, sub_obj_types, by='predicate')
+data_inv <- inner_join(data_inv, sub_obj_types, by=c('pred_inv' = 'predicate'))
+colnames(data_inv)[13:14] <- c('obj_type', 'sub_type')
+data_inv$sub_type[which(data_inv$sub_type == 'literal' | data_inv$sub_type == 'bnode')] <- 'Thing'
 
 ### get training labels
 counting <- read.csv('classifier_crowd_annotations/counting/figure-eight/f1411878_grouped.csv')
@@ -40,5 +51,6 @@ labelled_data_enumerating <- inner_join(data, enumerating[, c(1,9)], by='predica
 
 ### write to dataset
 write.csv(data, 'feature_file/predicates_p_50.csv', row.names = F)
+write.csv(data_inv, 'feature_file/inv_predicates_p_50.csv', row.names = F)
 write.csv(labelled_data_counting, 'feature_file/labelled_data_counting.csv', row.names = F)
 write.csv(labelled_data_enumerating, 'feature_file/labelled_data_enumerating.csv', row.names = F)
