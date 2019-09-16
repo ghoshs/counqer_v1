ab912267073b0cb8064ebb494c416005f7e2c8d0
ghoshs <ghoshs@mpi-inf.mpg.de>
2019-09-16 17:25:19 +0200

    script to get predicted predicate list

--- alignment/get_predicate_list.R
+++ alignment/get_predicate_list.R
@@ -9,5 +9,9 @@ write.csv(predC[, c(1)], 'alignment/counting.csv', row.names = F)
 predE <- read.csv('classifier/enumerating/predictions.csv')
 # predE$final <- ifelse(predE$linear+predE$bayesian+predE$neural+predE$lasso > 2, 1, 
                       # ifelse(predE$linear+predE$bayesian+predE$neural+predE$lasso < 2, 0, 0.5))
-predE <- predE[which(predE$linear == 1),]
+predE <- predE[which(predE$lasso == 1),]
 write.csv(predE[, c(1)], 'alignment/enumerating.csv', row.names = F)
+
+predE_inv <- read.csv('classifier/enumerating/predictions_inv.csv')
+predE_inv <- predE_inv[which(predE_inv$lasso == 1),]
+write.csv(predE_inv[, c(1)], 'alignment/enumerating_inv.csv', row.names = F)
